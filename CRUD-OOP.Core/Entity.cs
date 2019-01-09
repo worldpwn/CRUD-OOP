5091bacd6b6f24969de86e3c6b6327125ce31fdb
Andrey Knyazev <Andrey.Knyazev@firstlinesoft.onmicrosoft.com>
2019-01-09 11:39:34 +0300

    Added Initial Book Object and cover it with simple Unit Test.

---
+++ CRUD-OOP.Core/Entity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_OOP.Core
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    {
+
+        public TId Id { get; protected set; }
+
+        protected Entity(TId id)
+        {
+            if (object.Equals(id, default(TId)))
+            {
+                throw new ArgumentException("The ID cannot be the type's default value", "id");
+            }
+
+            this.Id = id;
+        }
+
+        public override bool Equals(object otherObject)
+        {
+            var entity = otherObject as Entity<TId>;
+            if (entity != null)
+            {
+                return this.Equals(entity);
+            }
+            return base.Equals(otherObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public bool Equals(Entity<TId> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Id.Equals(other.Id);
+        }
+    }
+}
---
+++ CRUD-OOP.Core/Objects/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_OOP.Core.Objects
+{
+    public class Book : Entity<Guid>
+    {
+        public int? IdInDb { get; private set; }
+        public string BookName { get; private set; }
+        public string Author { get; private set; }
+        public DateTimeOffset PublishedDate { get; private set; }
+        public int Pages { get; private set; }
+        public string ISBN { get; private set; }
+
+        private Book(Guid id, int? idInDb, string bookName, string author, DateTimeOffset publishedDate, int pages, string ISBN) : base(id: id)
+        {
+            this.IdInDb = idInDb;
+            this.BookName = bookName;
+            this.Author = author;
+            this.PublishedDate = publishedDate;
+            this.Pages = pages;
+            this.ISBN = ISBN;
+        }
+
+        public static Book Create(int? idInDb, string bookName, string author, DateTimeOffset publishedDate, int pages, string ISBN)
+        {
+            return new Book(
+                id: Guid.NewGuid(),
+                idInDb: idInDb,
+                bookName: bookName,
+                author: author,
+                publishedDate: publishedDate,
+                pages: pages,
+                ISBN: ISBN);
+        }
+    }
+}
